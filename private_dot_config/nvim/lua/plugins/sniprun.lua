b14ec1c0deb72d4e0ba55c04768fb333d69de655
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-10-03 22:04:39 -0400

    Update nvim

---
+++ private_dot_config/nvim/lua/plugins/sniprun.lua
@@ -0,0 +1,22 @@
+return {
+    "michaelb/sniprun",
+    branch = "master",
+
+    build = "sh install.sh",
+    -- do 'sh install.sh 1' if you want to force compile locally
+    -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+
+    config = function()
+        require("sniprun").setup({
+            display = { "Terminal" },
+            display_options = {
+                terminal_scrollback = vim.o.scrollback, -- change terminal display scrollback lines
+                terminal_line_number = false, -- whether show line number in terminal window
+                terminal_signcolumn = false, -- whether show signcolumn in terminal window
+                terminal_position = "vertical", --# or "horizontal", to open as horizontal split instead of vertical split
+                terminal_width = 45,          --# change the terminal display option width (if vertical)
+                terminal_height = 20,         --# change the terminal display option height (if horizontal)
+            },
+        })
+    end,
+}
