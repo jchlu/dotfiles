96d8d76db51f0dd80ff478551e03af8f853e3c54
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-10-29 12:29:07 -0400

    Add oil.nvim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,18 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
+    float = {
+      preview_split = "right",
+      override = function(conf)
+        return conf
+      end,
+    }
+  },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
