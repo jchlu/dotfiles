d7f191c1a70965b1a4d6b3b8a9b8749396b76d54
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-10-30 09:05:12 -0400

    Set nvim background to transparent

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -12,14 +12,20 @@ return {
   config = function(_, opts)
     require('catppuccin').setup(opts) -- Replace this with your favorite colorscheme
     vim.cmd("colorscheme catppuccin-macchiato") -- Replace this with your favorite colorscheme
+    vim.cmd [[
+      highlight Normal guibg=none
+      highlight NonText guibg=none
+      highlight Normal ctermbg=none
+      highlight NonText ctermbg=none
+    ]]      
     -- Colorscheme overrides
-    vim.cmd([[
-      autocmd VimEnter * hi DiffAdd guifg=#00FF00 guibg=#005500
-      autocmd VimEnter * hi DiffDelete guifg=#FF0000 guibg=#550000
-      autocmd VimEnter * hi DiffChange guifg=#CCCCCC guibg=#555555
-      autocmd VimEnter * hi DiffText guifg=#00FF00 guibg=#005500
-      autocmd VimEnter * hi LineNr guifg=#ff8e33
-    ]])
+    --vim.cmd([[
+    --      autocmd VimEnter * hi DiffAdd guifg=#00FF00 guibg=#005500
+    --      autocmd VimEnter * hi DiffDelete guifg=#FF0000 guibg=#550000
+    --      autocmd VimEnter * hi DiffChange guifg=#CCCCCC guibg=#555555
+    --      autocmd VimEnter * hi DiffText guifg=#00FF00 guibg=#005500
+    --      autocmd VimEnter * hi LineNr guifg=#ff8e33
+    --    ]])
   end
 }
 
