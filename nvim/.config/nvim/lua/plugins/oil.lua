b131caeeb56d87a1650ece56d88044dc935f4544
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-10-30 09:04:41 -0400

    Prevent oil hijacking all explorer tasks

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,8 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
+    -- Don't autostart
+    default_file_explorer = false,
     view_options = {
       -- Show files and directories that start with "."
       show_hidden = true,
