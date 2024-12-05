78b7f1886539efe5de5f51b25fb3d8b261b93d83
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-12-05 16:43:58 -0400

    Advent: The basic init.lua

---
+++ nvim/.config/nvimadvent/init.lua
@@ -0,0 +1,14 @@
+print('Advent of NeoVim')
+require('config.lazy')
+require('config.keymaps')
+-- require('advent.learninglua')
+
+vim.opt.shiftwidth     = 4
+vim.opt.number         = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc     = 'Highlight when yanking text',
+  group    = vim.api.nvim_create_augroup('advent-highlight-yank', { clear = true }),
+  callback = function() vim.highlight.on_yank() end,
+})
