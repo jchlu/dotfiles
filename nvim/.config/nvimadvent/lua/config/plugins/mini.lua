b560bff2225916fd77c4d2950b3e35fac5f53f2c
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-12-06 12:44:14 -0400

    Tidy the config

--- nvim/.config/nvimadvent/lua/config/plugins/mini.lua
+++ nvim/.config/nvimadvent/lua/config/plugins/mini.lua
@@ -2,9 +2,24 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
-     require('mini.statusline').setup { use_icons = true }
-     require('mini.align').setup()
-     require('mini.surround').setup()
-     require('mini.pairs').setup()
+    require('mini.align').setup()
+    require('mini.icons').setup()
+    require('mini.pairs').setup()
+    require('mini.statusline').setup { use_icons = true }
+    require('mini.surround').setup()
+    require('mini.files').setup({  -- Customization of explorer windows
+      windows = {
+	-- Maximum number of windows to show side by side
+	max_number = 3,
+	-- Whether to show preview of file/directory under cursor
+	preview = true,
+	-- Width of focused window
+	width_focus = 50,
+	-- Width of non-focused window
+	width_nofocus = 15,
+	-- Width of preview window
+	width_preview = 70,
+      }
+    })
   end
 }
