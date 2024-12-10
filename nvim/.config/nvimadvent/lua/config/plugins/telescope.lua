7eca7c3b962e54a7b1825986b5ef9379d564a0d1
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-12-10 19:05:01 -0400

    Add telescope, which-key and lazydev

--- nvim/.config/nvimadvent/lua/config/plugins/telescope.lua
+++ nvim/.config/nvimadvent/lua/config/plugins/telescope.lua
@@ -1,4 +1,22 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  config = function()
+    require('telescope').setup {
+      defaults = {
+        layout_strategy = 'horizontal',
+        layout_config = { height = 0.95, width = 0.95, preview_width = 0.6 },
+      },
+      --   pickers = {
+      --     find_files = {
+      --       theme = 'ivy',
+      --     },
+      --   },
+    }
+    require('telescope').load_extension('fzf')
+  end,
 }
