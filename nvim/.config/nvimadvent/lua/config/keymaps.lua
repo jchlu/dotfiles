7e8681bb2221582e05438ed715e9cad63e267033
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2024-12-19 11:19:06 -0400

    Setup more keymaps

--- nvim/.config/nvimadvent/lua/config/keymaps.lua
+++ nvim/.config/nvimadvent/lua/config/keymaps.lua
@@ -1,23 +1,33 @@
--- vim.keymap.set("n", "<space><space>x", ":source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = 'Execute the current line in Lua' })
-vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = 'Execute the current line in Lua' })
--- vim.keymap.set("n", "<space>ee", ":lua MiniFiles.open()<CR>")
-vim.keymap.set("n", "<space>ee", ":Oil<CR>", { desc = 'Opem the Oil file manager' })
-vim.keymap.set("n", "<C-s>", "<cmd>write<cr>")
-vim.keymap.set("i", "<C-s>", "<esc><cmd>write<cr>") -- write file in insert mode
-vim.keymap.set("n", "<C-t>", "<cmd>$tabnew<cr>", { desc = 'Open a new tab after the final one' })
-vim.keymap.set({ "n", "t" }, "<C-\\>", "<cmd>Floaterminal<CR>", { desc = 'Toggle a floating terminal window' })
+local keymap = vim.keymap
+-- keymap.set("n", "<space><space>x", ":source %<CR>")
+keymap.set("n", "<space>x", ":.lua<CR>", { desc = 'Execute the current line in Lua' })
+keymap.set("v", "<space>x", ":lua<CR>", { desc = 'Execute the current line in Lua' })
+-- keymap.set("n", "<space>ee", ":lua MiniFiles.open()<CR>")
+keymap.set("n", "<space>ee", ":Oil<CR>", { desc = 'Opem the Oil file manager' })
+keymap.set("n", "<C-s>", "<cmd>write<cr>")
+keymap.set("i", "<C-s>", "<esc><cmd>write<cr>", { desc = 'write file in insert mode' })
+keymap.set("n", "<C-t>", "<cmd>$tabnew<cr>", { desc = 'Open a new tab after the final one' })
+keymap.set({ "n", "t" }, "<C-\\>", "<cmd>Floaterminal<CR>", { desc = 'Toggle a floating terminal window' })
+
+-- Tab management
+keymap.set("n", "<leader>to", ":tabnew<CR>", { desc = 'open a new tab' })
+keymap.set("n", "<leader>tx", ":tabclose<CR>", { desc = 'close a tab' })
+keymap.set("n", "<leader>tn", ":tabn<CR>", { desc = 'next tab' })
+keymap.set("n", "<leader>tp", ":tabp<CR>", { desc = 'previous tab' })
 
 -- Telescope specific mappings
 local telescope_mappings = function()
   local telescope_check = vim.fn.exists('loaded_telescope')
   if not (telescope_check == 1) then return end
   local builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-  vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-  vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Telescope buffers' })
-  vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-  vim.keymap.set('n', '<leader>en', function()
+  keymap.set('n', '<leader>km', builtin.keymaps, { desc = 'View keymaps' })
+  keymap.set('n', '<leader>qf', builtin.quickfix, { desc = 'View quickfix' })
+  keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'View git status' })
+  keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+  keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+  keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Telescope buffers' })
+  keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+  keymap.set('n', '<leader>en', function()
     builtin.find_files {
       cwd = vim.fn.stdpath('config')
     }
