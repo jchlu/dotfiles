724e6ca576dc6975186c3e454db2166fdb33ff79
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2025-06-16 13:58:19 -0400

    formats

--- nvim/.config/nvimadvent/lua/config/plugins/lsp.lua
+++ nvim/.config/nvimadvent/lua/config/plugins/lsp.lua
@@ -40,7 +40,7 @@ return {
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if not client then return end
-        if client.supports_method('textDocument/formatting') then
+        if client:supports_method('textDocument/formatting') then
           vim.api.nvim_create_autocmd('BufWritePre', {
             buffer = args.buf,
             callback = function()
