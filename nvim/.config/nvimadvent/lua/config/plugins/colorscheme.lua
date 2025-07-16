e65c8efc6d2a87710ae5800d64c2044090799bdd
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2025-07-16 10:35:44 -0400

    Switch to gruvbox themes

--- nvim/.config/nvimadvent/lua/config/plugins/colorscheme.lua
+++ nvim/.config/nvimadvent/lua/config/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 return {
-  "folke/tokyonight.nvim",
-  -- dependencies = 'xiyaowong/transparent.nvim',
-  -- opts = { transparent = vim.g.transparent_enabled },
-  config = function(_, opts)
-    require('tokyonight').setup(opts)
-    vim.cmd.colorscheme "tokyonight-storm"
+  'ellisonleao/gruvbox.nvim',
+  priority = 1000,
+  config = function()
+    require('gruvbox').setup()
+    vim.cmd.colorscheme "gruvbox"
   end
 }
